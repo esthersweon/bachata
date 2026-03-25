c0b0cf7d85767aab267537f664198975370d45d5
Esther Weon <esther.s.weon@gmail.com>
2026-03-25 16:59:53 -0700

    Added icon for new "Frames" category

--- app/glossary/constants.ts
+++ app/glossary/constants.ts
@@ -4,12 +4,14 @@ import {
   CheckCircleIcon,
   FireIcon,
   SparklesIcon,
+  UsersIcon,
 } from "@heroicons/react/24/outline";
 
 export const categoriesToIcons: {
   [key: string]: React.ComponentType<{ className?: string }>;
 } = {
   All: CheckCircleIcon,
+  Frames: UsersIcon,
   Footwork: SparklesIcon,
   "Turns and spins": ArrowPathIcon,
   Isolations: ArrowsRightLeftIcon,
