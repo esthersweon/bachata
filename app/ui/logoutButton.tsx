8f06ce6f453ef5d90e7d06a937439da33323d1dd
Esther Weon <esther.s.weon@gmail.com>
2026-03-31 14:18:10 -0700

    Fix: Logout button functionality

--- app/ui/logoutButton.tsx
+++ app/ui/logoutButton.tsx
@@ -1,4 +1,5 @@
 import { signOut } from "@/auth";
+import { Button } from "@headlessui/react";
 import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
 
 export default function LogoutButton() {
@@ -9,10 +10,13 @@ export default function LogoutButton() {
 
   return (
     <form action={logout}>
-      <button className="flex h-12 grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+      <Button
+        type="submit"
+        className="flex items-center justify-center gap-2 bg-transparent! p-0! cursor-pointer!"
+      >
         <ArrowLeftStartOnRectangleIcon className="w-4" />
-        <div className="hidden md:block">Sign Out</div>
-      </button>
+        <div className="hidden md:block">Logout</div>
+      </Button>
     </form>
   );
 }
