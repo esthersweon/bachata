6db82e0c9ff2736a3b3fd3266d528fb7ce6b499f
Esther Weon <esther.s.weon@gmail.com>
2026-04-01 14:32:19 -0700

    Add Vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./globals.css";
 import LogoutButton from "./ui/logoutButton";
@@ -22,6 +23,7 @@ export default function RootLayout({
         </SideNav>
         <div className="w-full p-4 overflow-y-scroll">{children}</div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
